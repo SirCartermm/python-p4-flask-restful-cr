013706bdc2c076c7dbf8f0f6b16861382f43aa30
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-22 10:47:11 +0300

    Modified app.py

---
+++ server/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db, Newsletter
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
+class Home(Resource):
+
+    def get(self):
+
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+api.add_resource(Home, '/')
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
